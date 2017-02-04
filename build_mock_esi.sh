a46539eab213e3fc23b03b7d058c1726d5e4d763
antihax@gmail.com <antihax@gmail.com>
2017-02-04 09:35:51 -0600

    Build a little faster.

--- build_mock_esi.sh
+++ build_mock_esi.sh
@@ -8,7 +8,7 @@ java -jar swagger-codegen-cli.jar generate -DpackageName=esiDev -o ./dev -t ./mo
 java -jar swagger-codegen-cli.jar generate -DpackageName=esiLegacy -o ./legacy -t ./mock-esi-template -l go-server -i https://esi.tech.ccp.is/legacy/swagger.json?datasource=tranquility
 
 COUNTER=1
-while [  $COUNTER -lt 20 ]; do
+while [  $COUNTER -lt 5 ]; do
     rm ./v$COUNTER/*
     java -jar swagger-codegen-cli.jar generate -DpackageName=esiV$COUNTER -o ./v$COUNTER -t ./mock-esi-template -l go-server -i https://esi.tech.ccp.is/v$COUNTER/swagger.json?datasource=tranquility
     let COUNTER=COUNTER+1 
