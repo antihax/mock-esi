8cf2607dd46f98141f22dcce57103bde51c961f8
antihax@gmail.com <antihax@gmail.com>
2017-02-06 17:54:08 -0600

    update build script

--- build_mock_esi.sh
+++ build_mock_esi.sh
@@ -2,17 +2,18 @@
 rm ./latest/*
 rm ./dev/*
 rm ./legacy/*
-
+set -e
 java -jar swagger-codegen-cli.jar generate -DpackageName=esiLatest -o ./latest -t ./mock-esi-template -l go-server -i https://esi.tech.ccp.is/latest/swagger.json?datasource=tranquility
 java -jar swagger-codegen-cli.jar generate -DpackageName=esiDev -o ./dev -t ./mock-esi-template -l go-server -i https://esi.tech.ccp.is/dev/swagger.json?datasource=tranquility
 java -jar swagger-codegen-cli.jar generate -DpackageName=esiLegacy -o ./legacy -t ./mock-esi-template -l go-server -i https://esi.tech.ccp.is/legacy/swagger.json?datasource=tranquility
 
 COUNTER=1
 while [  $COUNTER -lt 5 ]; do
+    set +e
     rm ./v$COUNTER/*
+    set -e
     java -jar swagger-codegen-cli.jar generate -DpackageName=esiV$COUNTER -o ./v$COUNTER -t ./mock-esi-template -l go-server -i https://esi.tech.ccp.is/v$COUNTER/swagger.json?datasource=tranquility
     let COUNTER=COUNTER+1 
 done
 
-go get 
-go run main.go
+go test ./...
