283759df1a7849abb2d99efae7942a7e03826007
antihax <antihax@gmail.com>
2017-02-14 14:01:24 +0000

    rebuild esi at e121c69

--- latest/universe.go
+++ latest/universe.go
@@ -936,8 +936,22 @@ func GetUniverseStationsStationId(w http.ResponseWriter, r *http.Request) {
 	err = err
 
 	j := `{
-  "solar_system_id" : 30000142,
-  "station_name" : "Jita IV Moon IV - Caldari Navy Assembly Plant"
+  "max_dockable_ship_volume" : 50000000,
+  "name" : "Jakanerva III - Moon 15 - Prompt Delivery Storage",
+  "office_rental_cost" : 10000,
+  "owner" : 1000003,
+  "position" : {
+    "x" : 165632286720,
+    "y" : 2771804160,
+    "z" : -2455331266560
+  },
+  "race_id" : 1,
+  "reprocessing_efficiency" : 0.5,
+  "reprocessing_stations_take" : 0.05,
+  "services" : [ "courier-missions", "reprocessing-plant", "market", "repair-facilities", "fitting", "news", "storage", "insurance", "docking", "office-rental", "loyalty-point-store", "navy-offices" ],
+  "station_id" : 60000277,
+  "system_id" : 30000148,
+  "type_id" : 1531
 }`
 	vars := mux.Vars(r)
 	localV, err = processParameters(stationId, vars["station_id"])
@@ -1151,6 +1165,7 @@ func GetUniverseSystemsSystemId(w http.ResponseWriter, r *http.Request) {
 		localV interface{}
 		err error
 		systemId int32
+		language string
 		datasource string
 	)
 	// shut up warnings
@@ -1158,7 +1173,23 @@ func GetUniverseSystemsSystemId(w http.ResponseWriter, r *http.Request) {
 	err = err
 
 	j := `{
-  "solar_system_name" : "Jita"
+  "constellation_id" : 20000001,
+  "name" : "Akpivem",
+  "planets" : [ {
+    "moons" : [ 40000042 ],
+    "planet_id" : 40000041
+  }, {
+    "planet_id" : 40000043
+  } ],
+  "position" : {
+    "x" : -91174141133075340,
+    "y" : 43938227486247170,
+    "z" : -56482824383339900
+  },
+  "security_class" : "B",
+  "security_status" : 0.8462923765182495,
+  "stargates" : [ 50000342 ],
+  "system_id" : 30000003
 }`
 	vars := mux.Vars(r)
 	localV, err = processParameters(systemId, vars["system_id"])
@@ -1170,6 +1201,14 @@ func GetUniverseSystemsSystemId(w http.ResponseWriter, r *http.Request) {
 	if err := r.ParseForm(); err != nil {
 		errorOut(w, r, err)
 		return
+	}
+	if r.Form.Get("language") != "" {
+		localV, err = processParameters(language, r.Form.Get("language"))
+		if err != nil {
+			errorOut(w, r, err)
+			return
+		}
+		language = localV.(string)
 	}
 	if r.Form.Get("datasource") != "" {
 		localV, err = processParameters(datasource, r.Form.Get("datasource"))
