5991cbf2b5567264167f6c62c69298b0637e40b7
antihax <antihax@gmail.com>
2017-11-11 14:08:48 -0800

    just to make sure people know this is not real

--- login/login.go
+++ login/login.go
@@ -8,9 +8,9 @@ func Token(w http.ResponseWriter, r *http.Request) {
 
 	j := `{
 		"token_type":"bearer",
-		"access_token":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoiVlx1MDAxNcKbwoNUwoonbFPCu8KhwrYiLCJpYXQiOjE0NDQyNjI1NDMsImV4cCI6MTQ0NDI2MjU2M30.MldruS1PvZaRZIJR4legQaauQ3_DYKxxP2rFnD37Ip4",
+		"access_token":"VERYFAKETOKEN",
 		"expires_in":60,
-		"refresh_token":"fdb8fdbecf1d03ce5e6125c067733c0d51de209c"
+		"refresh_token":"THISISNOTAREALTOKEN"
 	}`
 
 	w.Header().Set("Content-Type", "application/json")
