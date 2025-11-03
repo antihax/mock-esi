3fb3382addee205ad07c3968bf8b3b7e0f3b6c66
jenkins <antihax@gmail.com>
2025-11-03 02:53:29 +0000

    rebuild esi at 22a9e76

--- main.go
+++ main.go
@@ -4,16 +4,6 @@ import (
 	"log"
 	"net/http"
 
-	_ "github.com/antihax/mock-esi/dev/go"
-	_ "github.com/antihax/mock-esi/latest/go"
-	_ "github.com/antihax/mock-esi/legacy/go"
-	_ "github.com/antihax/mock-esi/v1/go"
-	_ "github.com/antihax/mock-esi/v2/go"
-	_ "github.com/antihax/mock-esi/v3/go"
-	_ "github.com/antihax/mock-esi/v4/go"
-	_ "github.com/antihax/mock-esi/v5/go"
-	_ "github.com/antihax/mock-esi/v6/go"
-
 	"github.com/antihax/mock-esi/mockesi"
 )
 
